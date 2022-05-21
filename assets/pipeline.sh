0f3fc6a5b74d52a6beb322a5bb9811ed1cef6403
Ethan O <algorhythmic@outlook.co.nz>
2022-05-21 19:25:48 +1200

    Custom commit messages in pipeline.sh working (if this does)

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,3 +1,19 @@
+
+if [ $# -eq 0 ]
+then
+	echo "No commit message supplied, please enter a message as an argument"
+	echo "Usage: bash pipeline.sh [message]"
+	exit
+#Check if message is empty or too many arguments supplied
+#could potentially use another argument as the message after the title
+elif [ -z "$1" ] || [ $# -gt 1 ]
+then
+	echo "Please input exactly one commit message. Either it was empty or more than one were entered."
+	echo "Usage: bash pipeline.sh [message]"
+	exit
+#else all input correct
+fi
+
 echo "(1) Build the application"
 npm install
 
@@ -10,8 +26,9 @@ fi
 
 echo "(2) Release (commit to repo)"
 
-git commit -a -m "COMPX341-22A-A3 Committing from CI/CD Pipeline"
-#vi makes -a and -m invisible, which sucks, but they're still there so we good
+git commit -a -m "$1"
+
+#vi makes -a and -m invisible (at least in powershell oof), which sucks, but they're still there so we good
 
 echo "(3) Deploy application (on localhost)"
 npm run start
