697077adbadedbdf4dc2340753e3c1bdc374df95
Ethan O <algorhythmic@outlook.co.nz>
2022-05-21 14:20:17 +1200

    Pipeline updated. Figuring out commit message etc

--- assets/pipeline0.sh
+++ assets/pipeline0.sh
@@ -1,8 +1,25 @@
 echo "(1) Build the application"
 npm install
-npm run build
 
-echo "Deploy application (on localhost)"
+if npm run build; then
+	echo "Build succeeded"
+else
+	echo "Build Failed"
+	exit
+fi
+
+echo "(2) Release (commit and push(?) to repo)"
+# Originally was planning to push as well but now I think about it that kinda 
+	# doesn't make sense. We don't need (or want) to update the main project
+	# every time we run the CICD, but we may want to manually do it later on,
+	# so let's just commit.
+
+	#even though I went through all the trouble to set that up.
+
+git commit -a
+COMPX341-22A-A3 Commiting from CI/CD Pipeline
+
+echo "(3) Deploy application (on localhost)"
 npm run start
 
 echo "FIN"
